3eadd159007c8a276507cf6571d73e0707e74c57
Abby <ba40431@gmail.com>
2022-04-22 16:59:19 +0800

    初步頁面切版

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,9 +1,24 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const userAPI = express.Router();
 
+userAPI.use(bodyParser.json())
+
 userAPI.get('/user', (req, res) => {
-  res.send('ok');
+  if(JSON.stringify(req.body.data===null)) {throw 'error'};
+  let data = {
+    // 'id':
+    // 'name': 
+    // 'email':
+  };
+  res.send(JSON.stringify(data));
+});
+userAPI.post('/user', (req, res, next) => {
+  res.status(200);
+  let data = {
+    
+  };
+  res.send(JSON.stringify(data));
 });
 
 module.exports = userAPI;
